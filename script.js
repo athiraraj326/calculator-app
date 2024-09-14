b4f06392348f75034f8d9811dd263f15f22d0b25
Athira Raj <athiraraj326@gmail.com>
2024-09-14 10:11:04 +0530

    code pushed

---
+++ script.js
@@ -0,0 +1,18 @@
+const displayData = (content)=>{
+    result.value += content
+}
+
+const clearCalcScreen = ()=>{
+    result.value = ""
+}
+const evaluateExpr = ()=>{
+    try{
+        result.value = eval(result.value)
+    }
+    catch{
+        result.value = "Invalid!!!"
+    }
+}
+const removeLastCalcItem = ()=>{
+    result.value = result.value.slice(0,-1)
+}
